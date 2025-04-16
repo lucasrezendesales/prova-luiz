7dd78c6bd151d8ab4ee5048ed0b334eac118a9cc
lucasrezendesales <lucas.rezende.estagio@gmail.com>
2025-04-15 21:58:33 -0300

    aa

--- py.py
+++ py.py
@@ -1,4 +1,8 @@
-# Importações necessárias para usar o Selenium
+# Pra comecar, py -m pip install selenium  & tambem py -m pip install webdriver_manager
+#Apos isso arrumei o codigo para rodar
+
+
+#Modulos de importacao e invokacao do webdriver
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -7,16 +11,17 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-# Configuração do navegador Chrome
+
+# configura o webdriver
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.maximize_window()
 
 try:
-    # Acessa o endereço do site a ser testado
+    # Acessa o endereco do professor
     url = "https://www.hankeds.com.br/prova/login.html"
     driver.get(url)
 
-    # Espera 2 segundos para a página carregar
+    # Delay do professor de 2 segundos para carregar 
     time.sleep(2)
 
     # Função para digitar texto lentamente (simulando usuário real)
@@ -25,30 +30,35 @@ try:
             elemento.send_keys(letra)
             time.sleep(delay)
 
-    # Localizando elementos na página (usuário, senha e botão entrar)
+    # Localiza elementos na pagina usando um find_element pelo id atribuido no html e o xpath do buton por nao TER ID (usuário, senha e botão entrar)
     usuario = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "username")))
     senha = driver.find_element(By.ID, "password")
     botao = driver.find_element(By.XPATH, "//button[contains(text(), 'Entrar')]")
 
-    # Digitando o usuário lentamente
+    # Digita o usuario e da um sleep de 1 segundo
     digitar_lento(usuario, "admin")
     time.sleep(1)
 
+    # Digita a senha e da um sleep (dorme) 1 segundo
     digitar_lento(senha, "admin123456")
     time.sleep(1)
 
+    # Clica no botão para fazer login
     botao.click()
     time.sleep(4)
 
+    # Verifica se o redirecionamento foi feito corretamente
     if "destino.html" in driver.current_url:
         print("Teste passou: redirecionado corretamente.")
     else:
         print("Teste falhou: redirecionamento não ocorreu.")
 
+    # Aguarda um pouco antes de fechar o navegador (para printar evidências)
     time.sleep(5)
 
 except Exception as e:
     print("Erro durante o teste:", str(e))
 
 finally:
-    driver.quit()
+    # Killa o chrome deive apos rodar o script
+    driver.quit()
